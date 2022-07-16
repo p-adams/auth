285f9572e611c8190ae3cf4650e2f612306fa968
Paul Adams <ghusiyy@gmail.com>
2022-07-15 22:48:33 -0700

    remove dir

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/p-adams/workspace/cpp_sandbox/auth
-# Build directory: /Users/p-adams/workspace/cpp_sandbox/auth/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
